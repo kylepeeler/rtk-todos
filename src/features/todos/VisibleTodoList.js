669c9751ed21106de631f0c57f59656db854ee4f
Kyle Peeler <kylepeeler@gmail.com>
2020-06-07 13:44:04 -0400

    redid folder structure to use features rather than components/containers pattern

--- src/features/todos/VisibleTodoList.js
+++ src/features/todos/VisibleTodoList.js
@@ -2,7 +2,7 @@ import { connect } from 'react-redux';
 import { createSelector } from '@reduxjs/toolkit';
 import { toggleTodo } from 'features/todos/todosSlice';
 import { VisibilityFilters } from 'features/filters/filtersSlice';
-import TodoList from '../components/TodoList';
+import TodoList from './TodoList';
 
 const selectTodos = (state) => state.todos;
 const selectFilter = (state) => state.visibilityFilter;
